b2f1c29f3bb3a4431ae9790e3eb37d3ae3299f90
iamdevopser <107312874+iamdevopser@users.noreply.github.com>
2023-02-26 13:47:32 +0200

    Add files via upload

---
+++ Ders-1/Ders-1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders_1    // HELLO WORLD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
